d3e94d8c5c4d2dfff0f05ef52805494b7077dfc7
snchimata <38873813+snchimata@users.noreply.github.com>
2021-10-04 02:34:34 -0500

    Clean & train pipeline

---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+"""
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
+"""
+import argparse
+import logging
+import wandb
+import pandas as pd
+import os
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(job_type="basic_cleaning")
+    run.config.update(args)
+
+    # Download input artifact. This will also log that this script is using this
+    # particular version of the artifact
+    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+
+    ######################
+    # YOUR CODE HERE     #
+    ######################
+    logger.info("Downloading artifact")
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+
+    df = pd.read_csv(artifact_local_path)
+
+    # Drop outliers
+    logger.info("Drop Outliers")
+    idx = df['price'].between(args.min_price, args.max_price)
+    df = df[idx].copy()
+    
+    # Convert last_review to datetime
+    logger.info("Convert lastreview to datetime format")
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
+    # Drop outliers in longitude and latitude columns
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
+    # Save dataframe to file
+    logger.info(f'Save cleaned dataset to Weights & Biases {args.output_artifact}')
+    df.to_csv(args.output_artifact, index=False)
+
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    
+    artifact.add_file(args.output_artifact)
+    logger.info(f'Log artifact {args.output_artifact}')
+    run.log_artifact(artifact)
+    logger.info("Artifact uploaded")
+    os.remove(args.output_artifact)
+    
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
+
+
+    parser.add_argument(
+        "--input_artifact", 
+        type=str,
+        help="Input artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_artifact", 
+        type=str,
+        help="Output artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_type", 
+        type=str,
+        help="Output artifact type",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description", 
+        type=str,
+        help="Output artifact description",
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help="Minimum price range",
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type=float,
+        help="Maximum price range",
+        required=True
+    )
+
+
+    args = parser.parse_args()
+
+    go(args)
--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,17 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data):
+    """
+    Test size of the dataset is reasonable (not too small, not too large)
+    """
+    assert 15000 < data.shape[0] < 1000000
+    
+def test_price_range(data, min_price, max_price):
+    """
+    Test for proper price range
+    """
+    assert data['price'].between(min_price, max_price).all()
+
+
